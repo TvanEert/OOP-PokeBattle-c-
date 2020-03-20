3c556323ce3dc4fc5eec70f1f9018ee1d92bb494
Tim <99051246@mydavinci.nl>
2020-03-20 10:25:33 +0100

    V.1 Date : 18/03/20
    Pokemon class added and Pokemon can be created

---
+++ PokeBattle/PokeBattle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pokebattle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pokemon Femke = new Pokemon("Femke", "Water", 17, 8);
+
+            Femke.DisplayStats();
+            Console.Read();
+        }
+    }
+}
---
+++ PokeBattle/PokeBattle/pokemon.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokebattle
+{
+    class Pokemon
+    {
+        //Properties
+        public string Name { get; }
+        public string EnergyType { get; }
+        public int MaxHP { get; }
+        public int HP { get; set; }
+        public string Weakness { get; set; }
+        private double MultiplierWeak = 1.2;
+        public string Resistance { get; set; }
+        private double MultiplierRes = 0.8;
+
+        //Constructor
+        public Pokemon(string Name, string EnergyType, int MaxHP, int HP)
+        {
+            this.Name = Name;
+            this.EnergyType = EnergyType;
+            this.MaxHP = MaxHP;
+            this.HP = HP;
+            CalcWeakness();
+            CalcResistance();
+        }
+
+        //Methods
+        private void CalcWeakness()
+        {
+            switch (EnergyType)
+            {
+                case "Fire":
+                    this.Weakness = "Water";
+                    break;
+                case "Water":
+                    this.Weakness = "Grass";
+                    break;
+                case "Grass":
+                    this.Weakness = "Fire";
+                    break;
+            }
+        }
+
+        private void CalcResistance()
+        {
+            switch (EnergyType)
+            {
+                case "Fire":
+                    this.Resistance = "grass";
+                    break;
+                case "Grass":
+                    this.Resistance = "Water";
+                    break;
+                case "Water":
+                    this.Resistance = "Fire";
+                    break;
+            }
+        }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(EnergyType);
+            Console.WriteLine(MaxHP);
+            Console.WriteLine(HP);
+            Console.WriteLine(Weakness);
+            Console.WriteLine(Resistance);
+        }
+    }
+}
